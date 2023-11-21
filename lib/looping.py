38a08b3b2dc6f009435f92ccaed4861f8520e04c
Johey Hargrett <joheyhargrett@gmail.com>
2023-11-21 02:23:34 -0500

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
+#Write a function fizzbuzz() that prints the numbers from 1 to 100. 
+# For multiples of three, print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five, print "FizzBuzz".
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x**2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for x in range(1, 101):
+        output = ""
+        
+        if x % 3 == 0:
+            output += "Fizz"
+        
+        if x % 5 == 0:
+            output += "Buzz"
+        
+        if output == "":
+            output = str(x)
+        
+        print(output)
